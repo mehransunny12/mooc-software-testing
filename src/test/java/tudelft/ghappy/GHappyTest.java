277ca063d3119d0bdf261bf2cde39032963677d1
Sunny <mehran1.sunny@gmail.com>
2020-01-21 21:52:41 +0100

    Error Fixing in gHappy Class.

--- src/test/java/tudelft/ghappy/GHappyTest.java
+++ src/test/java/tudelft/ghappy/GHappyTest.java
@@ -1,4 +1,38 @@
 package tudelft.ghappy;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 public class GHappyTest {
+
+    private   GHappy gHappy;
+
+    @BeforeEach
+    public void getinitialized()
+    {
+         gHappy=new GHappy();
+    }
+
+    @Test
+    public void gHappy()
+    {
+
+        Assertions.assertTrue(gHappy.gHappy("ggxxggxx"));
+    }
+
+    @Test
+    public void gHappy1()
+    {
+
+        Assertions.assertFalse(gHappy.gHappy("xxgxx"));
+    }
+
+    @Test
+    public void gHappy2()
+    {
+
+        Assertions.assertFalse(gHappy.gHappy("xxggyygxx"));
+    }
+
 }
