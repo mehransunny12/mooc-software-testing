a72fa9e7e1c860dbea96ca6b369f6f96778aaa7c
Sunny <mehran1.sunny@gmail.com>
2020-01-28 19:08:53 +0100

    Mocking Exercise

--- src/test/java/tudelft/discount/DiscountApplierTest.java
+++ src/test/java/tudelft/discount/DiscountApplierTest.java
@@ -1,5 +1,27 @@
 package tudelft.discount;
 
-public class DiscountApplierTest {
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 
+import java.util.Arrays;
+import java.util.List;
+
+public class DiscountApplierTest {
+    @Test
+    public void discountFilterTest() {
+        Product bed=new Product("BED",10,"HOME");
+        Product desk=new Product("DESK",20,"BUSINESS");
+
+
+        ProductDao dao= Mockito.mock(ProductDao.class);
+
+        List<Product> result = Arrays.asList(bed,desk);
+        Mockito.when(dao.all()).thenReturn(result);
+
+        DiscountApplier discountApplier=new DiscountApplier(dao);
+        discountApplier.setNewPrices();
+        Assertions.assertEquals(10*0.9, result.get(0).getPrice());
+        Assertions.assertEquals(20*1.1, result.get(1).getPrice());
+    }
 }
